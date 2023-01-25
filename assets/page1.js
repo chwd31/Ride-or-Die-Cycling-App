d756c8a076442f8fbec622a5a64f0d1f00396a18
KarimB101 <karim.baptiste@yahoo.com>
2023-01-24 19:18:50 -0600

    clean slate

--- assets/page1.js
+++ assets/page1.js
@@ -1,1 +1,27 @@
 
+function getApi() {
+    var requestURL= "https://openweathermap.org/forecast5"
+
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //looping over the fetch response and inserting the URL of your repos into a list
+      for (var i = 0; i < data.length; i++) {
+        //Create a list element
+        var listItem = document.createElement('li');
+
+        //Set the text of the list element to the JSON response's .html_url property
+        listItem.textContent = data[i].html_url;
+
+        //Append the li element to the id associated with the ul element.
+        repoList.appendChild(listItem);
+      }
+    });
+
+    
+}
+
+console.log(requestURL)
+
